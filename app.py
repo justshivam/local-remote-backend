4907de1df875616924113d737fb44b349e91ba00
Shivam Singh <stdnt.shivam@gmai.com>
2023-06-17 21:52:54 +0530

    Implemented Basic API

---
+++ app.py
@@ -0,0 +1,67 @@
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
+from constants import (
+    PLAY_PAUSE_MUSIC,
+    VOLUME_UP,
+    VOLUME_DOWN,
+    VOLUME_MUTE,
+    NEXT_TRACK,
+    PREVIOUS_TRACK,
+    SLEEP_WINDOWS,
+)
+from details_api import (
+    volume_up,
+    volume_down,
+    volume_mute,
+    next_track,
+    play_pause_music,
+    previous_track,
+    sleep_windows,
+)
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+@app.route("/api/v1/")
+@cross_origin()
+def test_route():
+    return {"message": "test"}
+
+
+@app.route("/api/v1/action")
+def action_route():
+    requested_action = request.args.get("action")
+    executed = False
+    try:
+        if requested_action == PLAY_PAUSE_MUSIC:
+            play_pause_music()
+            executed = True
+        elif requested_action == VOLUME_UP:
+            volume_up()
+            executed = True
+        elif requested_action == VOLUME_DOWN:
+            volume_down()
+            executed = True
+        elif requested_action == VOLUME_MUTE:
+            volume_mute()
+            executed = True
+        elif requested_action == NEXT_TRACK:
+            next_track()
+            executed = True
+        elif requested_action == PREVIOUS_TRACK:
+            previous_track()
+            executed = True
+        elif requested_action == SLEEP_WINDOWS:
+            sleep_windows()
+            executed = True
+    except:
+        return {"message": f"Failed To Execute Request: {requested_action}"}, 500
+    if not executed:
+        return {"message": f"Unknown Request: {requested_action}"}, 400
+    return {"message": f"Executed Request: {requested_action}"}, 202
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ details_api.py
@@ -0,0 +1,65 @@
+from winsdk.windows.media.control import GlobalSystemMediaTransportControlsSessionManager as MediaManager
+import winsdk.windows.media.control as wmc
+from pynput.keyboard import KeyCode, Controller
+import asyncio
+import os
+
+keyboard = Controller()
+PLAY_PAUSE_MUSIC_KEY_CODE = 0xB3
+VOLUME_UP_KEY_CODE = 0xAF
+VOLUME_DOWN_KEY_CODE = 0xAE
+VOLUME_MUTE_KEY_CODE = 0xAD
+NEXT_TRACK_KEY = 0xB0
+PREVIOUS_TRACK_KEY = 0xB1
+
+def sleep_windows():
+    os.system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0")
+
+def play_pause_music():
+    keyboard.press(KeyCode.from_vk(PLAY_PAUSE_MUSIC_KEY_CODE))
+
+def volume_up():
+    keyboard.press(KeyCode.from_vk(VOLUME_UP_KEY_CODE))
+
+def volume_down():
+    keyboard.press(KeyCode.from_vk(VOLUME_DOWN_KEY_CODE))
+
+def volume_mute():
+    keyboard.press(KeyCode.from_vk(VOLUME_MUTE_KEY_CODE))
+
+def next_track():
+    keyboard.press(KeyCode.from_vk(NEXT_TRACK_KEY))
+
+def previous_track():
+    keyboard.press(KeyCode.from_vk(PREVIOUS_TRACK_KEY))
+
+async def get_media_info():
+    sessions = await MediaManager.request_async()
+
+    current_session = sessions.get_current_session()
+    if current_session:
+        info = await current_session.try_get_media_properties_async()
+
+        info_dict = {song_attr: info.__getattribute__(song_attr) for song_attr in dir(info) if song_attr[0] != '_'}
+
+        info_dict['genres'] = list(info_dict['genres'])
+
+        return info_dict
+    return None
+
+async def getMediaSession():
+    sessions = await wmc.GlobalSystemMediaTransportControlsSessionManager.request_async()
+    session = sessions.get_current_session()
+    return session
+
+def mediaIs(state):
+    session = asyncio.run(getMediaSession())
+    if session == None:
+        return False
+    return int(wmc.GlobalSystemMediaTransportControlsSessionPlaybackStatus["PLAYING"]) == session.get_playback_info().playback_status
+
+
+if __name__ == '__main__':
+    current_media_info = asyncio.run(get_media_info())
+    print(current_media_info)
+    print(mediaIs("PLAYING"))
